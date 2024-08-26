27cd09a4699d31ad27636a9d24ff6a62bb5aeb07
MithunJayant-M <147626913+MithunJayant-M@users.noreply.github.com>
2024-08-26 18:02:16 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = ({ cartCount }) => {
+    return (
+        <header>
+            <h1>Paradise Nursery</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/cart">
+                    Cart <span>({cartCount})</span>
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
